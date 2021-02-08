9a6d4f90ff67c55b09697fc80621ce2be4127174
Kristian Knudsen <knudsenkristian123@gmail.com>
2021-02-08 10:10:14 +0100

    Add and find box location

--- argos3-test/controller/test_controller/test_controller.cpp
+++ argos3-test/controller/test_controller/test_controller.cpp
@@ -5,23 +5,39 @@
 /* 2D vector definition */
 #include <argos3/core/utility/math/vector2.h>
 
+
+using namespace argos;
+
     test_controller::test_controller() :
         m_pcWheels(NULL),
-        m_fWheelVelocity(2.5f){}
-        // m_pcProximity(NULL),
-        // m_cAlpha(10.0f),
-        // m_fDelta(0.5f),
-        // m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
-        //                         ToRadians(m_cAlpha)) {}
+        m_fWheelVelocity(2.5f),
+        posSensor(NULL),
+        pcBox(NULL){}
 
     void test_controller::Init(TConfigurationNode& t_node) 
     {
-        m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+        m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+        posSensor = GetSensor<CCI_PositioningSensor>("positioning");
         GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
+
+        pcBox = new CBoxEntity("box1",                   // id
+                                CVector3(0.5, 0.5, 0.0), // position
+                                CQuaternion(),           // orientation
+                                true,                    // movable or not?
+                                CVector3(0.2, 0.2, 0.5), // size
+                                2.0);                    // mass in kg
+
+       AddEntity(*pcBox);
     }
 
     void test_controller::ControlStep()
     {
+        const CCI_PositioningSensor::SReading& robotPos = posSensor->GetReading();
+
+        LOG << "Box X: " << pcBox->GetEmbodiedEntity().GetOriginAnchor().Position.GetX() << std::endl;
+        LOG << "X: " << robotPos.Position.GetX() << std::endl;
+        LOG << "Y: " << robotPos.Position.GetY() << std::endl;
+
         m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
     }
 
