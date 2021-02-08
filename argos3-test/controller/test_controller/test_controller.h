55b4b786b02c8073f70a6d857f1ba23c3faae1a0
Kristian Knudsen <knudsenkristian123@gmail.com>
2021-02-08 10:12:00 +0100

    Added inheritance

--- argos3-test/controller/test_controller/test_controller.h
+++ argos3-test/controller/test_controller/test_controller.h
@@ -3,11 +3,14 @@
 
 #include <argos3/core/control_interface/ci_controller.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
-
+#include <argos3/plugins/robots/generic/control_interface/ci_positioning_sensor.h>
+#include <argos3/core/utility/logging/argos_log.h>
+#include <argos3/plugins/simulator/entities/box_entity.h>
+#include <argos3/core/simulator/loop_functions.h>
 
 using namespace argos;
 
-class test_controller : public CCI_Controller {
+class test_controller : public CCI_Controller, public CLoopFunctions{
 
 public:
 
@@ -15,11 +18,12 @@ public:
    ~test_controller(){}
 
    virtual void Init(TConfigurationNode& t_node);
-
    virtual void ControlStep();
 
 private:
    CCI_DifferentialSteeringActuator* m_pcWheels;
+   CCI_PositioningSensor* posSensor;
+   CBoxEntity* pcBox;
 
    Real m_fWheelVelocity;
 
