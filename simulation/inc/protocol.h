97c3d614b2b5a81346eab7d2f93e963a5a5137f2
Claus Huynh <claus.huynh@yahoo.dk>
2021-03-15 20:10:03 +0100

    we ccan now send more via the protocol

--- simulation/inc/protocol.h
+++ simulation/inc/protocol.h
@@ -6,36 +6,161 @@
 #include <argos3/core/utility/networking/tcp_socket.h>
 #include <argos3/core/utility/logging/argos_log.h>
 #include <argos3/core/utility/math/vector3.h>
+#include <opencv2/core.hpp>
+
 
 
 class protocol
 {
 public:
+    enum dataType
+    {
+        typeString,
+        typeCVector3,
+        typeCRadians,
+        typeReal,
+        typeCByteArray,
+        typePoint
+    };
+    
     protocol(protocol &&) = default;
     protocol(argos::CTCPSocket& socket);
     protocol();
     ~protocol();
+    void operator()(argos::CTCPSocket& socket);
+
+    /****************************************
+    ***** transmit part of the protocol *****
+    *****************************************/
+
+    /** @name send
+     * @brief Sends a datapackage
+     * @param message The data to send.
+     * @retval True if it has been sent false otherwise.
+     */
     bool send(argos::CByteArray message);
+
+    /** @name send
+     * @brief Sends a datapackage
+     * @param message The data to send.
+     * @retval True if it has been sent false otherwise.
+     */
     bool send(std::string message);
-    bool messageHasBeenSendt();
+
+    /** @name send
+     * @brief Sends a datapackage
+     * @param message The data to send.
+     * @retval True if it has been sent false otherwise.
+     */
+    bool send(argos::CVector3 message);
+
+    /** @name send
+     * @brief sends a datapackage
+     * @param X The radian to send.
+     * @param Y The radian to send.
+     * @param Z The radian to send.
+     * @retval True if it has been sent false otherwise.
+     */
+    bool send(argos::CRadians X, argos::CRadians Y, argos::CRadians Z);
+    
+    /** @name send
+     * @brief Sends a datapackage
+     * @param message The data to send.
+     * @retval True if it has been sent false otherwise.
+     */
+    bool send(argos::Real message); 
+    
+    /** @name send
+     * @brief Sends a datapackage
+     * @param message The data to send.
+     * @retval True if it has been sent false otherwise.
+     */
+    bool send(cv::Point message);
+
+
+    /****************************************
+    ****** recieve part of the protocol *****
+    *****************************************/
+
+
+    /** @name sendPosition
+     * @brief sends a position (legacy code please use @ref send)
+     * @param position The position to send.
+     * @retval true if it has been sent false otherwise.
+     */
     bool sendPosition(argos::CVector3 position);
 
+
+
+    /** @name recieve
+     * @brief recieves a message.
+     * @param message The buffer in which the message is saved.
+     * @retval true if it did recieve anyting else false.
+     */
     bool recieve(argos::CByteArray& message);
-    void operator()(argos::CTCPSocket& socket);
+
+    /** @name recieve
+     * @brief recieves a message.
+     * @param message The buffer in which the message is saved.
+     * @retval true if it did recieve anyting else false.
+     */
+    bool recieve(std::string& message);
+
+    
+    
+    /** @name recieve
+     * @brief recieves a message.
+     * @param message The buffer in which the message is saved.
+     * @retval true if it did recieve anyting else false.
+     */
+    bool recieve(argos::CVector3& message);
+    
+    /** @name recieve
+     * @brief recieves a message.
+     * @param message The buffer in which the message is saved.
+     * @retval true if it did recieve anyting else false.
+     */
+    bool recieve(argos::Real& message);
+    
+    /** @name recieve
+     * @brief recieves a message.
+     * @param message The buffer in which the message is saved.
+     * @retval true if it did recieve anyting else false.
+     */
+    bool recieve(cv::Point& message);
+    
+    /** @name recieve
+     * @brief recieves anyting without saving it
+     */
+    bool recieve();
+
+    
+    /** @name recieve
+     * @brief recieves a message.
+     * @param X The buffer for X.
+     * @param Y The buffer for Y.
+     * @param Z The buffer for Z.
+     * @retval true if it did recieve anyting else false.
+     */
+    bool recieve(argos::CRadians& X, argos::CRadians& Y, argos::CRadians& Z);
+
+
+    
     bool recievePosition(argos::CVector3& message);
 
 private:
-    /* data */
     argos::CTCPSocket* socket;
     bool sendt = true;
     bool recieved = true;
-    void sendMessage(argos::CByteArray message);
+    void sendMessage(argos::CByteArray message, dataType type);
+
     void recieveMessage();
-    void setUpServer();
-    void setUpClient();
+
     std::thread communicate;
     argos::CByteArray recievedMessage, recievedMessageLength;
-    argos::UInt8 messageLength[1];
+    argos::UInt8 messageLength[2];
+    argos::UInt8 preMessageRecieved[2];
+
 };
 
 
--- simulation/src/protocol.cpp
+++ simulation/src/protocol.cpp
@@ -13,11 +13,23 @@ protocol::protocol(){}
 
 protocol::~protocol(){}
 
+
+
+/****************************************
+***** transmit part of the protocol *****
+*****************************************/
+/* public send code they all spawn a thread */
+
+/** @name send
+ * @brief Sends a datapackage
+ * @param message The data to send.
+ * @retval True if it has been sent false otherwise.
+ */
 bool protocol::send(argos::CByteArray message) 
 {
     if (sendt && recieved)
     {
-        communicate = std::thread(&protocol::sendMessage, this, message);
+        communicate = std::thread(&protocol::sendMessage, this, message, dataType::typeCByteArray);
         communicate.detach();
         sendt = false;
         return true;
@@ -25,37 +37,126 @@ bool protocol::send(argos::CByteArray message)
     else
         return false;
 }
-bool protocol::send(std::string message_) 
+/** @name send
+ * @brief Sends a datapackage
+ * @param message The data to send.
+ * @retval True if it has been sent false otherwise.
+ */
+bool protocol::send(std::string message) 
 {
-    argos::CByteArray message = argos::CByteArray((argos::UInt8*)message_.c_str(), message_.size()+1);
-    return protocol::send(message);
-}
-
-void protocol::sendMessage(argos::CByteArray message)
-{
-    argos::UInt8 premessage[2];
-    premessage[0] = (message.Size());
-    while(socket->GetEvents().find(argos::CTCPSocket::EEvent::OutputReady) == socket->GetEvents().end())
+    if (sendt && recieved)
     {
-        ;
+        argos::CByteArray package;
+        package = argos::CByteArray((argos::UInt8*)message.c_str(), message.size()+1);
+        communicate = std::thread(&protocol::sendMessage, this, package, dataType::typeString);
+        communicate.detach();
+        sendt = false;
+        return true;
     }
-    socket->SendBuffer(premessage, 1);
-
-    while(socket->GetEvents().find(argos::CTCPSocket::EEvent::OutputReady) == socket->GetEvents().end())
-    {
-        ;
-    }
-    socket->SendByteArray(message);
-    sendt = true;
+    else
+        return false;
 }
-
-
-bool protocol::messageHasBeenSendt() 
+/** @name send
+ * @brief Sends a datapackage
+ * @param message The data to send.
+ * @retval True if it has been sent false otherwise.
+ */
+bool protocol::send(argos::CVector3 message) 
 {
-    return sendt;
+    if (sendt && recieved)
+    {        
+        std::string strMessage;
+        strMessage.append(std::to_string(message.GetX()) + ' ');
+        strMessage.append(std::to_string(message.GetY()) + ' ');
+        strMessage.append(std::to_string(message.GetZ()));
+
+        argos::CByteArray package;
+        package = argos::CByteArray((argos::UInt8*)strMessage.c_str(), strMessage.size()+1);
+        communicate = std::thread(&protocol::sendMessage, this, package, dataType::typeCVector3);
+        communicate.detach();
+        sendt = false;
+        return true;
+    }
+    else
+        return false;
+
+}
+/** @name send
+ * @brief sends a datapackage
+ * @param X The radian to send.
+ * @param Y The radian to send.
+ * @param Z The radian to send.
+ * @retval True if it has been sent false otherwise.
+ */
+bool protocol::send(argos::CRadians X, argos::CRadians Y, argos::CRadians Z) 
+{
+    if (sendt && recieved)
+    {        
+        std::string strMessage;
+        strMessage.append(std::to_string(X.GetValue()) + ' ');
+        strMessage.append(std::to_string(Y.GetValue()) + ' ');
+        strMessage.append(std::to_string(Z.GetValue()));
+
+        argos::CByteArray package;
+        package = argos::CByteArray((argos::UInt8*)strMessage.c_str(), strMessage.size()+1);
+        communicate = std::thread(&protocol::sendMessage, this, package, dataType::typeCRadians);
+        communicate.detach();
+        sendt = false;
+        return true;
+    }
+    else
+        return false;
+}
+/** @name send
+ * @brief Sends a datapackage
+ * @param message The data to send.
+ * @retval True if it has been sent false otherwise.
+ */
+bool protocol::send(argos::Real message) 
+{
+    if (sendt && recieved)
+    {   
+        u_char* prePackage = reinterpret_cast<u_char*>(&message);
+        argos::CByteArray package(prePackage, sizeof(prePackage)/sizeof(u_char));
+        communicate = std::thread(&protocol::sendMessage, this, package, dataType::typeReal);
+        communicate.detach();
+        sendt = false;
+        return true;
+    }
+    else
+        return false;
+}
+/** @name send
+ * @brief Sends a datapackage
+ * @param message The data to send.
+ * @retval True if it has been sent false otherwise.
+ */
+bool protocol::send(cv::Point message) 
+{
+    if (sendt && recieved)
+    {   
+        std::string strMessage;
+        strMessage.append(std::to_string(message.x) + ' ');
+        strMessage.append(std::to_string(message.y));
+
+        argos::CByteArray package;
+        package = argos::CByteArray((argos::UInt8*)strMessage.c_str(), strMessage.size()+1);
+        communicate = std::thread(&protocol::sendMessage, this, package, dataType::typePoint);
+        communicate.detach();
+        sendt = false;
+
+        return true;
+    }
+    else
+        return false;
 }
 
 
+/** @name sendPosition
+ * @brief sends a position (legacy code please use @ref send)
+ * @param position The position to send.
+ * @retval true if it has been sent false otherwise.
+ */
 bool protocol::sendPosition(argos::CVector3 position) 
 {
     std::string message;
@@ -66,6 +167,37 @@ bool protocol::sendPosition(argos::CVector3 position)
 }
 
 
+/** the private thread code **/
+void protocol::sendMessage(argos::CByteArray message, dataType type) 
+{
+    argos::UInt8 preMessage[2];
+    preMessage[0] = message.Size();
+    preMessage[1] = type;
+    while(socket->GetEvents().find(argos::CTCPSocket::EEvent::OutputReady) == socket->GetEvents().end())
+    {
+        ;
+    }
+    socket->SendBuffer(preMessage, 2);
+
+    while(socket->GetEvents().find(argos::CTCPSocket::EEvent::OutputReady) == socket->GetEvents().end())
+    {
+        ;
+    }
+    socket->SendByteArray(message);
+    sendt = true;
+}
+
+
+
+/****************************************
+****** recieve part of the protocol *****
+*****************************************/
+
+/** @name recieve
+ * @brief Recieves a message.
+ * @param message The buffer in which the message is saved.
+ * @retval True if it did recieve anyting else false.
+ */
 bool protocol::recieve(argos::CByteArray& message) 
 {
     // std::cout << "sendt: " << sendt << '\n';
@@ -77,43 +209,155 @@ bool protocol::recieve(argos::CByteArray& message)
         recieved = false;
     }
     else
-    {
         return false;
-    }
     usleep(500);
-    if (recieved)
+    if (recievedMessage.Size()>0)
     {
         message = recievedMessage;
+        recievedMessage.Clear();
         return true;
     }
     else
-    {
         return false;
-    }
-    
-    
 }
-
-void protocol::recieveMessage() 
+/** @name recieve
+ * @brief Recieves a message.
+ * @param message The buffer in which the message is saved.
+ * @retval True if it did recieve anyting else false.
+ */
+bool protocol::recieve(std::string& message) 
 {
-    while (socket->GetEvents().find(argos::CTCPSocket::EEvent::InputReady) == socket->GetEvents().end())
+    argos::CByteArray package;
+    bool packageRecieved = recieve(package);
+    if(packageRecieved && preMessageRecieved[1] == dataType::typeString)
     {
-        ;
+        package >> message;
+        return true;
     }
-        socket->ReceiveBuffer(messageLength, 1);
-
-    while (socket->GetEvents().find(argos::CTCPSocket::EEvent::InputReady) == socket->GetEvents().end())
+    return false;
+}
+/** @name recieve
+ * @brief Recieves a message.
+ * @param message The buffer in which the message is saved.
+ * @retval True if it did recieve anyting else false.
+ */
+bool protocol::recieve(argos::CVector3& message) 
+{
+    argos::CByteArray package;
+    bool packageRecieved = recieve(package);
+    if(packageRecieved && preMessageRecieved[1] == dataType::typeCVector3)
     {
-        ;
-    }
-        argos::CByteArray temp(messageLength[0]);
-        socket->ReceiveByteArray(temp);
-        recievedMessage = temp;
-    
+        std::string str_message;
+        package >> str_message;
 
-    recieved = true;
+        argos::Real X = std::stod(str_message.substr(0, str_message.find(' ')));
+        str_message.erase(0, str_message.find(' ') + 1);
+
+        argos::Real Y = std::stod(str_message.substr(0, str_message.find(' ')));
+        str_message.erase(0, str_message.find(' ') + 1);
+
+        argos::Real Z = std::stod(str_message.substr(0, str_message.find(' ')));
+        
+        message.SetX(X);
+        message.SetY(Y);
+        message.SetZ(Z);
+        return true;
+    }
+
+    //should not get here if everything went well
+    return false;
+}
+/** @name recieve
+ * @brief Recieves a message.
+ * @param message The buffer in which the message is saved.
+ * @retval True if it did recieve anyting else false.
+ */
+bool protocol::recieve(argos::Real& message) 
+{
+    argos::CByteArray package;
+    bool packageRecieved = recieve(package);
+    if(packageRecieved && preMessageRecieved[1] == dataType::typeReal)
+    {
+        double preMessage = *reinterpret_cast<double*>(package.ToCArray());
+        message = preMessage;
+        return true;
+    }
+    return false;
+}
+/** @name recieve
+ * @brief Recieves a message.
+ * @param message The buffer in which the message is saved.
+ * @retval True if it did recieve anyting else false.
+ */
+bool protocol::recieve(cv::Point& message) 
+{
+    argos::CByteArray package;
+    bool packageRecieved = recieve(package);
+    if(packageRecieved && preMessageRecieved[1] == dataType::typePoint)
+    {
+        std::string str_message;
+        package >> str_message;
+
+        int X = std::stod(str_message.substr(0, str_message.find(' ')));
+        str_message.erase(0, str_message.find(' ') + 1);
+
+        int Y = std::stod(str_message.substr(0, str_message.find(' ')));
+
+        message.x = X;
+        message.y = Y;
+        return true;
+    }
+    return false;
+}
+/** @name recieve
+ * @brief recieves anyting without saving it
+ */
+bool protocol::recieve()
+{
+    argos::CByteArray nothing;
+    return recieve(nothing);
+} 
+
+/** @name recieve
+ * @brief Recieves a message.
+ * @param X The buffer for X.
+ * @param Y The buffer for Y.
+ * @param Z The buffer for Z.
+ * @retval True if it did recieve anyting else false.
+ */
+bool protocol::recieve(argos::CRadians& X_, argos::CRadians& Y_, argos::CRadians& Z_) 
+{
+    argos::CByteArray package;
+    bool packageRecieved = recieve(package);
+    if(packageRecieved && preMessageRecieved[1] == dataType::typeCRadians)
+    {
+        std::string str_message;
+        package >> str_message;
+
+        argos::Real X = std::stod(str_message.substr(0, str_message.find(' ')));
+        str_message.erase(0, str_message.find(' ') + 1);
+
+        argos::Real Y = std::stod(str_message.substr(0, str_message.find(' ')));
+        str_message.erase(0, str_message.find(' ') + 1);
+
+        argos::Real Z = std::stod(str_message.substr(0, str_message.find(' ')));
+        
+        X_.SetValue(X);
+        Y_.SetValue(Y);
+        Z_.SetValue(Z);
+        return true;
+    }
+
+    //should not get here if everything went well
+    return false;
 }
 
+
+/** @name recievePosition
+ * @brief Recieves a argos::CVector3 (legacy code please use @ref recieve).
+ * @param position The buffer in which the position is saved.
+ * @retval True if it did recieve anyting else false.
+ */
 bool protocol::recievePosition(argos::CVector3& position) 
 {
     argos::CByteArray message;
@@ -137,4 +381,19 @@ bool protocol::recievePosition(argos::CVector3& position)
     position.SetZ(Z);
     
     return true;
+}
+
+
+/** the private thread code **/
+void protocol::recieveMessage() 
+{
+    while (socket->GetEvents().find(argos::CTCPSocket::EEvent::InputReady) == socket->GetEvents().end());
+    socket->ReceiveBuffer(preMessageRecieved, 2);
+
+    while (socket->GetEvents().find(argos::CTCPSocket::EEvent::InputReady) == socket->GetEvents().end());
+    argos::CByteArray temp(preMessageRecieved[0]);
+
+    socket->ReceiveByteArray(temp);
+    recievedMessage = temp;
+    recieved = true;
 }
